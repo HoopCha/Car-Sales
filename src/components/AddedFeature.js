b76ab4638b8f6d8d4f60b7a01fa0316bb1d13428
Chase Hooper <Hoopchaser@gmail.com>
2019-11-06 10:57:23 -0700

    mvp mostly

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeBuy } from '../actions/actions';
 
 const AddedFeature = props => {
+  const removeItem = event => {
+    event.preventDefault();
+    props.removeBuy(props.feature);
+  };
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button"
+       onClick={removeItem}
+      >X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(() => {}, { removeBuy })(AddedFeature);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buyItem } from '../actions/actions';
 
 const AdditionalFeature = props => {
+  const addItem = event => {
+    event.preventDefault();
+    props.buyItem(props.feature);
+  };
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"
+      onClick={addItem}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(() => {}, { buyItem })(AdditionalFeature);
